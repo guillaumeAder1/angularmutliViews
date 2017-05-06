f967d95d3a0a1118d567b23769d29bb4dfd0a24c
guillaumeAder1 <guillaume.ader1@gmail.com>
2017-05-06 17:06:34 +0100

    add ui router

--- app/components/list/listcontroller.js
+++ app/components/list/listcontroller.js
@@ -9,25 +9,15 @@
  */
 angular.module('testControllerViewsApp')
     .controller('ListcontrollerCtrl', function($scope, getBikes) {
-        $scope.toto = "ojpo";
-
-        //$scope.data = ["oooo", "uuuuu", "eeeeee", "oooooo"];
-
-        $scope.getText = function() {
-            return "totototot";
-        }
-
         $scope.getData = (function() {
             getBikes.getListBike().then(function(res) {
-                console.log(res);
                 $scope.data = res.data;
             }, function(err) {
-                console.log(err);
+                console.log("Query error::", err);
             });
         })();
 
-        $scope.logme = function(val) {
-            console.log(val.item.name);
-            this.$parent.$parent.selected = "totot";
+        $scope.selectItem = function(index) {
+            console.log($scope.data[index]);
         };
     });
